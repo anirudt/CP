db8f799eb281cc9498ab4f832d7e5577457de4af
anirudt <anirudt@gmail.com>
2016-04-22 08:45:14 +0530

    added minuting to temp:

--- temp.c
+++ temp.c
@@ -2,10 +2,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 typedef vector<int> vi;
+typedef vector<float> vf;
+typedef vector<string> vs;
+
+typedef map<int, int> mii;
 
 int main() {
 
