358fa4835c6030e73b7d9c40d546d8ef1f46a8f0
anirudt <anirudt@gmail.com>
2016-04-28 18:06:35 +0530

    started 608, AC 10469

---
+++ 10469.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<float> vf;
+typedef vector<string> vs;
+typedef vector<vi> vi2d;
+typedef vector<ll> vll;
+
+typedef map<int, int> mii;
+
+// Glob Var Defs
+
+int main() {
+  int a, b;
+  while(scanf("%d %d", &a, &b) != EOF) {
+    int c = a^b;
+    cout << c << endl;
+  }
+
+  return 0;
+}
