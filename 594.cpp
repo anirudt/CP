7e17a0834a08a9c34be53d14b3156c59e9d1e747
anirudt <anirudt@gmail.com>
2016-04-28 08:16:24 +0530

    started 594, AC 665

---
+++ 594.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<float> vf;
+typedef vector<string> vs;
+typedef vector<vi> vi2d;
+typedef vector<ll> vll;
+
+typedef map<int, int> mii;
+
+// Glob Var Defs
+
+int main() {
+  int x;
+  scanf("%d", &x);
+  x = (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
+  x = (((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2));
+  x = (((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4));
+  x = (((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8));
+  int y = ((x >> 16) | (x << 16));
+  cout << y << endl;
+}
