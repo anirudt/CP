23f1a4164bba94dceaf40d78526c3cd6b5c11735
anirudt <anirudt@gmail.com>
2016-05-06 08:38:36 +0530

    added template changes, AC 575

--- 575.cpp
+++ 575.cpp
@@ -1,12 +1,12 @@
 #include <cstdio>
-#include <cmath>
-#include <climits>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
 #include <algorithm>
 #include <stack>
+#include <climits>
+#include <cmath>
 
 using namespace std;
 
@@ -23,6 +23,17 @@ typedef map<int, int> mii;
 // Glob Var Defs
 
 int main() {
-
+  string str;
+  while(getline(cin, str)) {
+    vi num;
+    int sum = 0, pdt = 1;
+    for (int i=str.size()-1; i>=0; i--) {
+      int k = str[i]-48;
+      pdt = pow(2, str.size()-i)-1;
+      sum += k*pdt;
+    }
+    if (sum == 0) break;
+    else cout << sum << endl;
+  }
   return 0;
 }
