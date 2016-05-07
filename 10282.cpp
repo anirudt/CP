00ad0d6ba8b9bcdd37c9d1fd969b4d4807d9a3ca
anirudt <anirudt@gmail.com>
2016-05-07 20:18:57 +0530

    AC 10282, 10226

--- 10282.cpp
+++ 10282.cpp
@@ -24,6 +24,21 @@ typedef map<int, int> mii;
 // Glob Var Defs
 
 int main() {
+  string s1, s2, line;
+  map<string, string> dict;
+  /* Accept input for dict */
+  while(getline(cin, line) && line.size()>0) {
+    istringstream iss(line);
+    iss >> s1;
+    iss >> s2;
+    dict[s2] = s1;
+  }
 
+  while(getline(cin, line)) {
+    if (dict[line].compare("") == 0)
+      cout << "eh" << endl;
+    else
+      cout << dict[line] << endl;
+  }
   return 0;
 }
